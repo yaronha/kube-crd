a3977761efe3dec1058d576acc1176343bbc9d81
danma <danma@yunify.com>
2019-01-17 17:21:31 +0800

    Update to new k8s version, manage vendor with glide.

--- crd/crd.go
+++ crd/crd.go
@@ -66,6 +66,7 @@ type Example struct {
 	Spec               ExampleSpec   `json:"spec"`
 	Status             ExampleStatus `json:"status,omitempty"`
 }
+
 type ExampleSpec struct {
 	Foo string `json:"foo"`
 	Bar bool   `json:"bar"`
@@ -81,6 +82,44 @@ type ExampleList struct {
 	meta_v1.TypeMeta `json:",inline"`
 	meta_v1.ListMeta `json:"metadata"`
 	Items            []Example `json:"items"`
+}
+
+func (in *Example) DeepCopyInto(out *Example) {
+    out.TypeMeta = in.TypeMeta
+    out.ObjectMeta = in.ObjectMeta
+    out.Spec = ExampleSpec{
+		Foo: in.Spec.Foo,
+		Bar: in.Spec.Bar,
+		Baz: in.Spec.Baz,
+	}
+	out.Status = ExampleStatus{
+		State: in.Status.State,
+		Message: in.Status.Message,
+    }
+}
+
+// DeepCopyObject returns a generically typed copy of an object
+func (in *Example) DeepCopyObject() runtime.Object {
+    out := Example{}
+    in.DeepCopyInto(&out)
+
+    return &out
+}
+
+// DeepCopyObject returns a generically typed copy of an object
+func (in *ExampleList) DeepCopyObject() runtime.Object {
+    out := ExampleList{}
+    out.TypeMeta = in.TypeMeta
+    out.ListMeta = in.ListMeta
+
+    if in.Items != nil {
+        out.Items = make([]Example, len(in.Items))
+        for i := range in.Items {
+            in.Items[i].DeepCopyInto(&out.Items[i])
+        }
+    }
+
+    return &out
 }
 
 // Create a  Rest client with the new CRD Schema
